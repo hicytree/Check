85f0bd235b1053b0e30779c8cbbcba4804465211
Jeffrey Liu <yjeffliu@gmail.com>
2023-09-13 03:22:18 -0700

    added mapview and focus

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,33 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
-const HomeScreen = () => {
-    return (
-        <View style={styles.container}>
-          <Text>Hello World!</Text>
-        </View>
-      );
+const HomeScreen = ({longitude}) => {
+    const [location, setLocation] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        })();
+    }, []);
+
+    if (location) {
+        return (
+            <View style={styles.container}>
+                <MapView 
+                    initialRegion={{
+                        latitude: location["coords"]["latitude"],
+                        longitude: location["coords"]["longitude"],
+                        latitudeDelta: 0.004,
+                        longitudeDelta: 0.003
+                    }}
+                    mapType="hybrid"
+                    style={styles.map} 
+                />
+            </View>
+        );
+    }
 }
 
-export default HomeScreen
-
+export default HomeScreen;
+    
 const styles = StyleSheet.create({
-     container: {
+    container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-     },
-     button: {
-        backgroundColor: "#0782F9",
-        width: '60%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-        marginTop: 40
     },
-    buttonText: {
-        color: 'white',
-        fontWeight: 700,
-        fontSize: 16,
+    map: {
+        width: '100%',
+        height: '100%',
     },
-})
+});
